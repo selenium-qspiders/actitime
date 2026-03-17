bef6e259b5f1f663b7747f4a85f5859b41ae7e0b
selenium-qspiders <prakashjyoti.jp18@gmail.com>
2026-03-17 13:00:16 +0530

    new demo test case added

---
+++ src/test/java/com/actitime/tests/TC_002.java
@@ -0,0 +1,13 @@
+package com.actitime.tests;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TC_002 
+{
+	@Test
+	public void demo()
+	{
+		Reporter.log("this is a demo test case" , true);
+	}
+}
